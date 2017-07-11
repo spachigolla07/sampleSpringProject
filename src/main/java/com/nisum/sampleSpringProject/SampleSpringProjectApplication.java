1d87b59b10ded9afe3047b69a5d3fcd3e30f4f9a
nisum <nisum@nisums-MacBook-Pro-5.local>
2017-07-11 07:05:13 -0700

    Added prin statement

--- src/main/java/com/nisum/sampleSpringProject/SampleSpringProjectApplication.java
+++ src/main/java/com/nisum/sampleSpringProject/SampleSpringProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleSpringProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("haii");
 		SpringApplication.run(SampleSpringProjectApplication.class, args);
 	}
 }
